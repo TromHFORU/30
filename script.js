b927345b27244fe8cf663cbf72ba0c4014bce57c
Roman Thapa <125963925+TromHFORU@users.noreply.github.com>
2026-06-30 20:34:16 +0545

    Updated code

--- script.js
+++ script.js
@@ -11,13 +11,53 @@ let meter = document.getElementById("result-meter")
 let kilo = document.getElementById("result-kilo")
 let litre = document.getElementById("result-litre")
 
+//Testing ternary operator
+const playerGuess = 3
+const correctAnswer = 6
 
+const message = playerGuess < correctAnswer ? 'Too Low' :
+playerGuess > correctAnswer ? 'Too High':'Exactly Correct'
 
+console.log(message)
+
+//Testing Switch
+
+function selectItem(item){
+    let price = 0
+    switch(item){
+        case 'coffee':
+            price = 2
+            break
+        case 'coke':
+            price = 3
+            break
+        case 'salad':
+            price = 4
+            break
+        default:
+            return `Sorry, we don't sell ${item}`
+    }
+    return `You selected ${item}.That will be $${price}`
+}
+console.log(selectItem('apple'))
+
+const favouriteDoungha = {
+    title : "Renegade Immortal",
+    year : 2023,
+    genre : "Action"
+}
+
+const{title,year,genre} = favouriteDoungha
+
+console.log(`My favourie doughua is ${title} and it was released in ${year} and genre of ${genre}.`)
 
 
 
 
 btn.addEventListener("click",function(){
+    
+        
+    
     const user =Number(input.value)
     let feet = (user * 3.281).toFixed(3)
     let gallon =(user * 0.264).toFixed(3)
@@ -26,6 +66,11 @@ btn.addEventListener("click",function(){
     meter.textContent = `${user} Meter | ${feet} Feet`
     kilo.textContent = `${user} Kilogram | ${pound} Pound`
     litre.textContent = `${user} Litre | ${gallon} Gallon`
-})
+    
+    
+        
+        
+    }
+)
 
 
