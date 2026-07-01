f4e7d2a9b278d8be3933c777535a6efbf12aa7e9
Roman Thapa <125963925+TromHFORU@users.noreply.github.com>
2026-07-01 12:03:17 +0545

    Updated

--- script.js
+++ script.js
@@ -11,48 +11,6 @@ let meter = document.getElementById("result-meter")
 let kilo = document.getElementById("result-kilo")
 let litre = document.getElementById("result-litre")
 
-//Testing ternary operator
-const playerGuess = 3
-const correctAnswer = 6
-
-const message = playerGuess < correctAnswer ? 'Too Low' :
-playerGuess > correctAnswer ? 'Too High':'Exactly Correct'
-
-console.log(message)
-
-//Testing Switch
-
-function selectItem(item){
-    let price = 0
-    switch(item){
-        case 'coffee':
-            price = 2
-            break
-        case 'coke':
-            price = 3
-            break
-        case 'salad':
-            price = 4
-            break
-        default:
-            return `Sorry, we don't sell ${item}`
-    }
-    return `You selected ${item}.That will be $${price}`
-}
-console.log(selectItem('apple'))
-
-// object destructuring
-const favouriteDoungha = {
-    title : "Renegade Immortal",
-    year : 2023,
-    genre : "Action"
-}
-
-const{title,year,genre} = favouriteDoungha
-
-console.log(`My favourie doughua is ${title} and it was released in ${year} and genre of ${genre}.`)
-
-
 
 
 btn.addEventListener("click",function(){
@@ -73,5 +31,4 @@ btn.addEventListener("click",function(){
         
     }
 )
-
 
